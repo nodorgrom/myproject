2a0ac2ca667264c97761df1419ee6ab663ea0bef
Evgenii Anisimov <nodorgrom@gmail.com>
2025-10-25 19:32:36 +0300

    migrate: add flask migrate

--- app/models/post.py
+++ app/models/post.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 from ..extensions import db
 
 class MyPost(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	subject = db.Column(db.String(250))
+	price = db.Column(db.String(250))
+	name = db.Column(db.String(250))
+	count = db.Column(db.String(250))
+	date = db.Column(db.DateTime, default=datetime.utcnow)
