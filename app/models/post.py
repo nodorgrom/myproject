c8b1abe0709b0e8605b0d844b3ec51944882281b
Evgenii Anisimov <nodorgrom@gmail.com>
2025-10-25 20:20:50 +0300

    templates: create; routes: post.create; static.css: create

--- app/models/post.py
+++ app/models/post.py
@@ -3,7 +3,7 @@ from ..extensions import db
 
 class MyPost(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	price = db.Column(db.String(250))
-	name = db.Column(db.String(250))
-	count = db.Column(db.String(250))
+	teacher = db.Column(db.String(250))
+	subject = db.Column(db.String(250))
+	student = db.Column(db.String(250))
 	date = db.Column(db.DateTime, default=datetime.utcnow)
--- app/routes/post.py
+++ app/routes/post.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
 from ..extensions import db
 from ..models.post import MyPost
 
@@ -6,10 +6,22 @@ my_post = Blueprint('my_post', __name__)
 
 
 
-@my_post.route('/my_post/<subject>')
-def create_subject(subject):
-	my_post = MyPost(subject=subject)
-	db.session.add(my_post)
-	db.session.commit()
-	return f"Post {my_post} created!"
+@my_post.route('/post/create', methods=['POST', 'GET'])
+def create():
+	if request.method == 'POST':
+		teacher = request.form.get('teacher')
+		subject = request.form.get('subject')
+		student = request.form.get('student')
 
+		post = MyPost(teacher=teacher, subject=subject, student=student)
+
+		try:
+			db.session.add(post)	
+			db.session.commit()
+			return redirect('/')
+		except Exception as e:
+			print(str(e))
+			raise e
+
+	else:
+		return render_template('post/create.html')
