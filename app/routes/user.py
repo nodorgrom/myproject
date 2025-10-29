b3b064c4a8f1d72eac6c1acf7ab82fa2addc591b
Evgenii Anisimov <nodorgrom@gmail.com>
2025-10-29 23:54:45 +0300

    route: user; add flash messages

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, flash
 from ..forms import RegistrationForm
 from ..extensions import db, bcrypt
 from ..models.user import MyUsers
@@ -12,17 +12,21 @@ my_users = Blueprint('my_users', __name__)
 def register_user():
 	form = RegistrationForm()
 
-	if form.validate_on_submit():
-		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		avatar_filename = save_picture(form.avatar.data)
-		user = MyUsers(user_name=form.user_name.data, login=form.login.data, avatar=avatar_filename, password=hashed_password)
+	if form.user_name.data and form.login.data and form.password.data != "":
+		if form.validate_on_submit():
+			hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+			avatar_filename = save_picture(form.avatar.data)
+			user = MyUsers(user_name=form.user_name.data, login=form.login.data, avatar=avatar_filename, password=hashed_password)
 
-		db.session.add(user)
-		db.session.commit()
+			db.session.add(user)
+			db.session.commit()
 
-		return redirect('/')
-	else:
-		print('Ошибка регистрации')
+			flash(f"Добро пожаловать, {form.user_name.data} ({form.login.data})!", "success")
+
+			return redirect('/')
+		else:
+			flash(f"Ошибка регистрации: {form.user_name.data} {form.login.data} {form.avatar.data}", "danger")
+			print('Ошибка регистрации')
 
 	return render_template('user/register.html', form=form)
 
